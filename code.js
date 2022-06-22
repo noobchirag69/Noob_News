3cae85f746213c2f450f46578471f143fa97b7a1
noobchirag69 <chiragchakraborty48@gmail.com>
2022-06-22 12:15:39 +0530

    Completed

--- code.js
+++ code.js
@@ -1,10 +1,32 @@
-const apiKey = '364993d049084ce98c88c51585b1464e';
-const url = `https://newsapi.org/v2/everything?q=superman&apiKey=${apiKey}`;
+function getNews() {
 
-let req = new Request(url);
+    $('.posts').text("");
 
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
+    let keyword = $('#keyword').val();
+
+    const apiKey = '364993d049084ce98c88c51585b1464e';
+    const url = `https://newsapi.org/v2/everything?q="${keyword}"&from=2022-06-21&sortBy=popularity&language=en&apiKey=${apiKey}`;
+
+    $.get(url, (response) => {
+        for (i = 0; i < response.articles.length; i++) {
+            let html = `<div class="card m-3">
+                            <div class="row g-0">
+                                <div class="col-md-4 d-flex justify-content-center">
+                                    <img src="${response.articles[i].urlToImage}" class="img-fluid" alt="Image Missing :/">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h6 class="card-title">${response.articles[i].title}</h6>
+                                        <p class="card-text">${response.articles[i].description}</p>
+                                        <hr>
+                                        <p class="source">Source: ${response.articles[i].source.name}</p class="source">
+                                        <a href="${response.articles[i].url}" target="_blank" class="btn btn-sm btn-dark">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`;
+
+            $('.posts').append(html);
+        }
     });
-
+}
