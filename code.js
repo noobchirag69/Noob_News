b193fac5fc561715f756332adbc396b44e58d412
noobchirag69 <chiragchakraborty48@gmail.com>
2022-06-23 13:00:43 +0530

    Added Searching Feature

--- code.js
+++ code.js
@@ -1,28 +1,62 @@
 const apiKey = 'GmFMdYk8V0s9KK6yKnkYlfXcAcv2xEvV';
-const url = `https://api.nytimes.com/svc/topstories/v2/world.json?api-key=${apiKey}`;
 
-fetch(url)
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-        for (i = 1; i < data.results.length; i++) {
-            let html = `<div class="card m-3">
+defaultNews();
+
+function defaultNews() {
+    const url = `https://api.nytimes.com/svc/topstories/v2/world.json?api-key=${apiKey}`;
+
+    fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            for (i = 2; i < data.results.length; i++) {
+                let html = `<div class="card m-3">
                             <div class="row g-0">
                                 <div class="col-md-4 d-flex justify-content-center">
-                                    <img src="${data.results[i].multimedia[0].url}" class="img-fluid" alt="Image Missing :/">
+                                    <img src="${data.results[i].multimedia[0].url}" class="img-fluid">
                                 </div>
                                 <div class="col-md-8">
                                     <div class="card-body">
                                         <h5 class="card-title">${data.results[i].title}</h5>
                                         <p class="card-text">${data.results[i].abstract}</p>
                                         <p class="source">Source: The New York Times</p>
-                                        <a href="${data.results[i].url}" target="_blank" class="btn btn-dark">Read More</a>
+                                        <a href="${data.results[i].url}" target="_blank" class="btn btn-dark btn-sm">Read More</a>
                                     </div>
                                 </div>
                             </div>
                         </div>`;
-            $('.posts').append(html);
-        }
-    })
+                $('.posts').append(html);
+            }
+        })
+}
+
+function getNews() {
+    $('.posts').text("");
+    let keyword = $('#keyword').val();
+    const search_url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${keyword}&api-key=${apiKey}`;
+    fetch(search_url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            for (i = 0; i < data.response.docs.length; i++) {
+                let html = `<div class="card m-3">
+                                <div class="row g-0">
+                                    <div class="col-md-4 d-flex justify-content-center">
+                                        <img src="https://static01.nyt.com/${data.response.docs[i].multimedia[0].url}" class="img-fluid">
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title">${data.response.docs[i].snippet}</h5>
+                                            <p class="card-text">${data.response.docs[i].lead_paragraph}</p>
+                                            <p class="source">Source: The New York Times</p>
+                                            <a href="${data.response.docs[i].web_url}" target="_blank" class="btn btn-dark btn-sm">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>`;
+                $('.posts').append(html);
+            }
+        })
+}
